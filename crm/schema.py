04e8268eff3c73dff235cb780a9b0bfd485502e3
FredrickMbithi <FredrickMbithi000@gmail.com>
2025-10-19 21:02:01 +0300

    Add CreateCustomer mutation for CRM GraphQL API

--- crm/schema.py
+++ crm/schema.py
@@ -10,8 +10,26 @@ class CustomerType(DjangoObjectType):
         fields = ("id", "name", "email", "phone")
 
 
+class CreateCustomer(graphene.Mutation):
+    class Arguments:
+        name = graphene.String(required=True)
+        email = graphene.String(required=True)
+        phone = graphene.String(required=True)
+
+    customer = graphene.Field(CustomerType)
+
+    def mutate(self, info, name, email, phone):
+        customer = Customer(name=name, email=email, phone=phone)
+        customer.save()
+        return CreateCustomer(customer=customer)
+
+
 class Query(graphene.ObjectType):
     all_customers = graphene.List(CustomerType)
 
     def resolve_all_customers(root, info):
         return Customer.objects.all()
+
+
+class Mutation(graphene.ObjectType):
+    create_customer = CreateCustomer.Field()
