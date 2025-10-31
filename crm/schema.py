a88cb379f43a8956c19f83aeb37a0bd4cfd0f174
ghost <fredrickmbithi000@gmail.com>
2025-10-31 04:18:43 +0300

    Corrected an error

--- crm/schema.py
+++ crm/schema.py
@@ -2,6 +2,7 @@ import graphene
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 from graphene import relay
+from crm.models import Product
 
 from .models import Customer, Product
 from .filters import CustomerFilter
